dfdee93520bb7120705c890a20ceaf43922fb135
Suttipong Srikok <fisca2012@gmail.com>
2021-06-19 19:14:10 +0700

    New Lines

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
-    cout << "I am learning C++";
+    cout << "Hello World! \n\n";
+    cout << "I am learning C++" << endl;
+    cout << "Nice to meet";
     return 0;
 }
+
+// Both \n and endl are used to break lines.
