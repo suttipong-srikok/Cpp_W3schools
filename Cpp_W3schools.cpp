f438b33d1b913a3d36a4064e7d0b210c51b38094
Suttipong Srikok <fisca2012@gmail.com>
2021-06-19 19:46:13 +0700

    Variables

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,9 +3,28 @@ using namespace std;
 
 int main()
 {
-    // This is a comment
-    /* The code below will print the words Hello World!
-    to the screen, and it is amazing */
-    cout << "Hello World!"; // This is a comment
+    // Declaring (Creating) Variables
+    int myNum = 5;              // Integer (whole number without decimals)
+    double myFloatNum = 5.99;   // Floating point number (with decimals)
+    char myLetter = 'D';        // Character
+    string myText = "Hello";    // String (text)
+    bool myBoolean = true;      // Boolean (true or false)
+    char myCArray[30] = "This is the Array of char";
+    cout << "myCArray = " << myCArray << endl;
+
+    //Display Variables
+    int myAge = 35;
+    cout << "I am " << myAge << " years old.\n";
+
+    // Add Variables Together
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    cout << sum << endl;
+
+    // Declare Multiple Variables
+    int u = 5, v = 6, z = 50;
+    cout << u + v + z;
+
     return 0;
 }
