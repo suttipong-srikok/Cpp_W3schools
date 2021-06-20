0ee0f5c812a49f2bfef8b53e4b4fe6e1a899eadd
Suttipong Srikok <fisca2012@gmail.com>
2021-06-20 19:30:47 +0700

    String Concatenation

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 int main()
 {
-    int x = 5;
+    /* String Concatenation */
+    // you can concatenate strings with the + operator:
+    string firstName = "John ";
+    string lastName = "Doe";
+    string fullName = firstName + lastName;
+    cout << fullName << "\n";
 
-    // Logical and
-    // Returns true if both statements are true
-    cout << (x > 3 && x < 10) << "\n";
-    // returns true (1) because 5 is greater than 3 AND 5 is less than 10
+    // you could also add a space with quotes (" " or ' '):
+    firstName = "Tom";
+    lastName = "Hanks";
+    fullName = firstName + " " + lastName;
+    cout << fullName << "\n";
 
-    // Logical or
-    // Returns true if one of the statements is true
-    cout << (x > 3 || x < 4) << "\n";
-    // returns true (1) because one of the conditions are true
-    // (5 is greater than 3, but 5 is not less than 4)
-
-    // Logical not
-    // Reverse the result, returns false if the result is true
-    cout << (!(x > 3 && x < 10)) << "\n";
-    // returns false (0) because ! (not) is used to reverse the result
+    /* Append */
+    // you can also concatenate strings with the append() function:
+    firstName = "Brian ";
+    lastName = "Greene";
+    fullName = firstName.append(lastName);
+    cout << fullName << "\n";
+    cout << "firstName after append is " << firstName << "\n";
 
     return 0;
 }
