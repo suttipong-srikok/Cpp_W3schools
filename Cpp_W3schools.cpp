2829c13a778cf48186dbf4957b99586de2fbea56
Suttipong Srikok <fisca2012@gmail.com>
2021-06-20 14:51:37 +0700

    Data Types

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 int main()
 {
-    int x;
-    cout << "Type a number: "; // Type number and press enter
-    cin >> x; // Get user input from the keyboard
-    cout << "Your number is: " << x; // Display the input value
+    // Creating variable
+    int myNum = 5;              // Integer (whole number
+    float myFloatNum = 5.99;    // Floating point number
+    double myDoubleNum = 9.98;  // Floating point number
+    char myLetter = 'D';        // Charactor
+    bool myBoolean = true;      // Boolean
+    string myText = "Hello";    // String
+
+    // Print variable values
+    cout << "int: " << myNum << "\n";
+    cout << "float: " << myFloatNum << "\n";
+    cout << "double: " << myDoubleNum << endl;
+    cout << "char: " << myLetter << endl;
+    cout << "bool: " << myBoolean << "\n";
+    cout << "string: " << myText << "\n";
 
     return 0;
 }
