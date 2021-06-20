24330df5733833bd70b7a7b875b6366d78d27cef
Suttipong Srikok <fisca2012@gmail.com>
2021-06-20 16:37:21 +0700

    Boolean Types

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,20 +3,10 @@ using namespace std;
 
 int main()
 {
-    int myNum = 1000;
-    cout << "myNum = " << myNum << "\n";
-
-    float myFloat = 5.43210123;
-    cout << "myFloat = " << myFloat << "\n";
-
-    double myDouble = 19.98765432;
-    cout << "myDouble = " << myDouble << "\n";
-
-    // Scientific Numbers
-    float f1 = 35e3;
-    double d1 = 12E4;
-    cout << "f1 = " << f1 << endl;
-    cout << "d1 = " << d1;
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun << "\n";    // Outputs 1 (true)
+    cout << isFishTasty;            // Outputs 0 (false)
 
     return 0;
 }
