9972f1cf3548c4b5e3d19c30a6e11e19515a0599
Suttipong Srikok <fisca2012@gmail.com>
2021-06-20 17:18:35 +0700

    Arithmetic Operators

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
 using namespace std;
 
+void printXY(int x, int y)
+{
+    cout << "x = " << x << ", y = " << y << "\n";
+}
+
 int main()
 {
-    // Create a string variable
-    string greeting = "Hello";
+    int sum1 = 100 + 50;    // 150 (100 + 50)
+    int sum2 = sum1 + 250;  // 400 (150+ 250)
+    int sum3 = sum2 + sum2; // 800 (400 + 400)
+    cout << sum1 << "\n";
+    cout << sum2 << "\n";
+    cout << sum3 << "\n\n";
 
-    // Output string value
-    cout << greeting;
+    /* Arithmetic Operators */
+    int x = 5, y = 3;
+    printXY(x, y);
+
+    // Addition (+)
+    cout << "x + y = " << x + y << "\n"; // Adds together two values
+
+    // Subtraction (-)
+    cout << "x - y = " << x - y << "\n"; // Subtracts one value from another
+
+    // Multiplication (*)
+    cout << "x * y = " << x * y << "\n"; // Multiplies two values
+
+    // Division (/)
+    x = 12;
+    printXY(x, y);
+    cout << "x / y = " << x / y << "\n"; // Divides one value by another
+
+    // Modulus (%)
+    x = 5, y = 2;
+    printXY(x, y);
+    cout << "x % y = " << x % y << "\n"; // Returns the division remainder
+
+    // Increment (++)
+    cout << "++x = " << ++x << "\n"; // Increases the value of a variable by 1
+
+    // Decrement (--)
+    cout << "--y = " << --y; // Decreases the value of a variable by 1
 
     return 0;
 }
