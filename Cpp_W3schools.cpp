d1e29f5d82eb8a23d5d7f9fbe62bec33ca1b367b
Suttipong Srikok <fisca2012@gmail.com>
2021-06-20 20:31:13 +0700

    Omitting Namespace

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
-    // It is possible to use the extraction operator >> on cin to display a string entered by a user:
-    //string firstName;
-    //cout << "Type your first name: ";
-    //cin >> firstName;
-    //cout << "Your name is: " << firstName << "\n\n";
-    // However, cin considers a space (whitespace, tabs, etc) as a terminating character, which means that it can only display a single word (even if you type many words)
-
-    // use the getline() function to read a line of text
-    string fullName;
-    cout << "Type your full name: ";
-    getline(cin, fullName);
-    cout << "Your name is: " << fullName << "\n";
+    // The using namespace std line can be omitted and replaced with the std keyword, followed by the :: operator
+    std::string greeting = "Hello";
+    std::cout << greeting << std::endl;
 
     return 0;
 }
