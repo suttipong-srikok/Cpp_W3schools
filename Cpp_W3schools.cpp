b4fa40aca753ef51200c2c4779cba509a6787e85
Suttipong Srikok <fisca2012@gmail.com>
2021-06-24 23:53:00 +0700

    The if Statement

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -4,14 +4,11 @@ using namespace std;
 
 int main()
 {
-    int x = 10, y = 9;
-    cout << (x > y) << endl; // returns 1 (true), because 10 is higher than 9
-
-    cout << (10 > 9) << endl; // returns 1 (true), because 10 is higher than 9
-
-    cout << (x == 10) << endl; // return 1 (true), because the vauel of x is equal to 10
-
-    cout << (10 == 15) << endl; //returns 0 (false), because 10 is not equal to 15
-
+    int x = 20;
+    int y = 18;
+    if (x > y)
+    {
+        cout << "x is greater than y";
+    }
     return 0;
 }
