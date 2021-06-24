7e25eaf75494007ab1cdcc30113a938a70e539fb
Suttipong Srikok <fisca2012@gmail.com>
2021-06-24 23:55:59 +0700

    The else Statement

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main()
 {
-    int x = 20;
-    int y = 18;
-    if (x > y)
+    int time = 20;
+    if (time < 18)
     {
-        cout << "x is greater than y";
+        cout << "Good day";
     }
+    else
+    {
+        cout << "Good evening.";
+    }
+
     return 0;
 }
