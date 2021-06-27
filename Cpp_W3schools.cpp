64fc815800ed2e5a481e31ad23d5d78319387a36
Suttipong Srikok <fisca2012@gmail.com>
2021-06-27 15:39:13 +0700

    Switch Statements

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,12 +3,37 @@ using namespace std;
 
 int main()
 {
-    // Short Hand If...Else (Ternary Operator)
-    // variable = (condition) ? expressionTrue : expressionFalse;
+    // Switch Statements
+    // Use the switch statement to select one of many code blocks to be executed.
 
-    int time = 22;
-    string result = (time < 18) ? "Good day." : "Good evening.";
-    cout << result << endl;
+    // The example below uses the weekday number to calculate the weekday name:
+
+    int day = 4;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    }
+    // Outputs "Thursday" (day 4)
 
     return 0;
 }
