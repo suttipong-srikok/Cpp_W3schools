13b00c9132a0a89239d9417e3163cad3d31bf7a3
Suttipong Srikok <fisca2012@gmail.com>
2021-06-27 15:44:26 +0700

    The default Keyword

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,37 +3,22 @@ using namespace std;
 
 int main()
 {
-    // Switch Statements
-    // Use the switch statement to select one of many code blocks to be executed.
-
-    // The example below uses the weekday number to calculate the weekday name:
+    // The default Keyword
+    // The default keyword specifies some code to run if there is no case match:
 
     int day = 4;
     switch (day)
     {
-    case 1:
-        cout << "Monday";
-        break;
-    case 2:
-        cout << "Tuesday";
-        break;
-    case 3:
-        cout << "Wednesday";
-        break;
-    case 4:
-        cout << "Thursday";
-        break;
-    case 5:
-        cout << "Friday";
-        break;
     case 6:
         cout << "Saturday";
         break;
     case 7:
         cout << "Sunday";
         break;
+    default:
+        cout << "Looking forward to the Weekend";
     }
-    // Outputs "Thursday" (day 4)
+    // Outputs "Looking forward to the Weekend"
 
     return 0;
 }
