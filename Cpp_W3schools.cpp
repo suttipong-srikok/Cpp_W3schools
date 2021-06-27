f8bdd5366ae7d44b27d5e43021199c76e405e563
Suttipong Srikok <fisca2012@gmail.com>
2021-06-27 16:19:38 +0700

    Break

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,22 +3,17 @@ using namespace std;
 
 int main()
 {
-    // For Loop
-    // When you know exactly how many times you want to loop through a block of code, use the for loop instead of a while loop:
+    // C++ Break
+    // The break statement can also be used to jump out of a loop.
 
-    //The example below will print the numbers 0 to 4:
-    /*
-    for (int i = 0; i < 5; i++)
+    // This example jumps out of the loop when i is equal to 4:
+
+    for (int i = 0; i < 10; i++)
     {
-        cout << i << "\n";
-    }
-    */
-
-    // Another Example
-    // This example will only print even values between 0 and 10:
-
-    for (int i = 0; i <= 10; i = i + 2)
-    {
+        if (i == 4)
+        {
+            break;
+        }
         cout << i << "\n";
     }
 
