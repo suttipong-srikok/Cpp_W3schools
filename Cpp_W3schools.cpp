b1461f807da2687e36b911bf243332798da5425e
Suttipong Srikok <fisca2012@gmail.com>
2021-07-04 00:40:39 +0700

    Modify the Pointer Value

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,19 +3,31 @@ using namespace std;
 
 int main()
 {
-    // C++ Dereference
-    // Get Memory Address and Value
+    // Modify the Pointer Value
 
-    // You can use the pointer to get the value of the variable, by using the * operator (the dereference operator):
+    // You can also change the pointer's value.
+    // But note that this will also change the value of the original variable:
 
-    string food = "Pizza";  // Variable declaration
-    string* ptr = &food;    // Pointer declaration
+    string food = "Pizza";
+    string* ptr = &food;
 
-    // Reference: Output the memory address of food with the pointer
-    cout << ptr << "\n";
+    // Output the value of food (Pizza)
+    cout << food << "\n";
 
-    // Dereference: Output the value of food with the pinter (Pizza)
+    // Output the memory address of food
+    cout << &food << "\n";
+
+    // Access the memory address of food and output its value
     cout << *ptr << "\n";
+
+    // Change the value of the pointer
+    *ptr = "Hamburger";
+
+    // Output the new value of the pointer (Hamburger)
+    cout << *ptr << "\n";
+
+    // Output the new value of the food variable (Hamburger)
+    cout << food << "\n";
 
     return 0;
 }
