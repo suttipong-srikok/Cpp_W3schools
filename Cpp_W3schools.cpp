c98f0d7cf56b5c661a5719d71fd82b48e3aafa91
Suttipong Srikok <fisca2012@gmail.com>
2021-07-03 19:00:11 +0700

    Omit Elements on Declaration

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,20 +3,14 @@ using namespace std;
 
 int main()
 {
-    // Omit Array Size
-    // You don't have to specify the size of the array. But if you don't, it will only be as big as the elements that are inserted into it:
+    // Omit Elements on Declaration
 
-    //string cars[] = {"Volvo", "BMW", "Ford"}; // size of array is always 3
+    // It is also possible to declare an array without specifying the elements on declaration, and add them later:
 
-    // This is completely fine. However, the problem arise if you want extra space for future elements. Then you have to overwrite the existing values.
-
-    // If you specify the size however, the array will reserve the extra space:
-
-    //string cars[5] = {"Volvo", "BMW", "Ford"}; // size of array is 5, even though it's only three elements inside it
-
-    // Now you can add a fourth and fifth element without overwriting the others:
-
-    string cars[5] = {"Volvo", "BMW", "Ford"};
+    string cars[5];
+    cars[0] = "Volvo";
+    cars[1] = "BMW";
+    cars[2] = "Ford";
     cars[3] = "Mazda";
     cars[4] = "Tesla";
     for (int i = 0; i < 5; i++)
