0fbbf6ab8d797b26ef48afeca829ec88a657cb78
Suttipong Srikok <fisca2012@gmail.com>
2021-07-04 00:34:38 +0700

    Dereference

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,27 +3,19 @@ using namespace std;
 
 int main()
 {
-    // Creating Pointers
+    // C++ Dereference
+    // Get Memory Address and Value
 
-    // A pointer however, is a variable that stores the memory address as its value.
-    // A pointer variable points to a data type (like int or string) of the same type, and is created with the * operator. The address of the variable you're working with is assigned to the pointer:
+    // You can use the pointer to get the value of the variable, by using the * operator (the dereference operator):
 
-    string food = "Pizza";  // A food variable of type string
-    string* ptr = &food;    // A pinter variable, with the name ptr, that stores the address of food
+    string food = "Pizza";  // Variable declaration
+    string* ptr = &food;    // Pointer declaration
 
-    // Output the value of food (Pizza)
-    cout << food << "\n";
-
-    // Output the memory address of food
-    cout << &food << "\n";
-
-    // Output the memory address of food with the pointer
+    // Reference: Output the memory address of food with the pointer
     cout << ptr << "\n";
+
+    // Dereference: Output the value of food with the pinter (Pizza)
+    cout << *ptr << "\n";
 
     return 0;
 }
-
-// Tip: There are three ways to declare pointer variables, but the first way is preferred:
-// string* mystring; // Preferred
-// string *mystring;
-// string * mystring;
