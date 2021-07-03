f8fca79cbaf7e03e9edfdaf8c358a0227895a45c
Suttipong Srikok <fisca2012@gmail.com>
2021-07-03 18:57:33 +0700

    Omit Array Size

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,12 +3,25 @@ using namespace std;
 
 int main()
 {
-    // Loop Through an Array
+    // Omit Array Size
+    // You don't have to specify the size of the array. But if you don't, it will only be as big as the elements that are inserted into it:
 
-    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
-    for (int i = 0; i < 4; i++)
+    //string cars[] = {"Volvo", "BMW", "Ford"}; // size of array is always 3
+
+    // This is completely fine. However, the problem arise if you want extra space for future elements. Then you have to overwrite the existing values.
+
+    // If you specify the size however, the array will reserve the extra space:
+
+    //string cars[5] = {"Volvo", "BMW", "Ford"}; // size of array is 5, even though it's only three elements inside it
+
+    // Now you can add a fourth and fifth element without overwriting the others:
+
+    string cars[5] = {"Volvo", "BMW", "Ford"};
+    cars[3] = "Mazda";
+    cars[4] = "Tesla";
+    for (int i = 0; i < 5; i++)
     {
-        cout << i << ": " << cars[i] << "\n";
+        cout << cars[i] << "\n";
     }
 
     return 0;
