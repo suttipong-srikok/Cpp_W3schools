453b2d23a2b0bb272db68aa4ab3366ec1e1a6be8
Suttipong Srikok <fisca2012@gmail.com>
2021-07-04 00:48:47 +0700

    Create and Call a Function

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,33 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// Create a function
+void myFunction()
+{
+    cout << "I just got executed!\n";
+}
+
 int main()
 {
-    // Modify the Pointer Value
-
-    // You can also change the pointer's value.
-    // But note that this will also change the value of the original variable:
-
-    string food = "Pizza";
-    string* ptr = &food;
-
-    // Output the value of food (Pizza)
-    cout << food << "\n";
-
-    // Output the memory address of food
-    cout << &food << "\n";
-
-    // Access the memory address of food and output its value
-    cout << *ptr << "\n";
-
-    // Change the value of the pointer
-    *ptr = "Hamburger";
-
-    // Output the new value of the pointer (Hamburger)
-    cout << *ptr << "\n";
-
-    // Output the new value of the food variable (Hamburger)
-    cout << food << "\n";
+    myFunction(); // call the function
+    myFunction();
+    myFunction();
 
     return 0;
 }
