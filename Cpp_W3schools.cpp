05b504ee9a8a3395e99349ad240ca3eecd4e1004
Suttipong Srikok <fisca2012@gmail.com>
2021-07-04 01:06:23 +0700

    Parameters and Arguments

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,20 +1,30 @@
+// Parameters and Arguments
+
+// Information can be passed to functions as a parameter.
+// Parameters act as variables inside the function.
+// Parameters are specified after the function name, inside the parentheses.
+
+// The following example has a function that takes a string called fname as parameter.
+// When the function is called, we pass along a first name,
+// which is used inside the function to print the full name:
+
 #include <iostream>
 using namespace std;
 
-// Function declaration
-void myFunction();
+void myFunction(string fname)
+{
+    cout << fname << "\n";
+}
 
 int main()
 {
-    myFunction(); // call the function
-    myFunction();
-    myFunction();
+    myFunction("Liam");
+    myFunction("Jenny");
+    myFunction("Anna");
 
     return 0;
 }
 
-// Function definition
-void myFunction()
-{
-    cout << "I just got executed!\n";
-}
+// When a parameter is passed to the function, it is called an argument.
+// So, from the example above: fname is a parameter,
+// while Liam, Jenny and Anna are arguments.
