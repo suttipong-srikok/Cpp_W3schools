3b68c44a42871988e77f61fc776403fb09366209
Suttipong Srikok <fisca2012@gmail.com>
2021-07-04 01:11:03 +0700

    Default Parameter Value

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,30 +1,26 @@
-// Parameters and Arguments
+// Default Parameter Value
 
-// Information can be passed to functions as a parameter.
-// Parameters act as variables inside the function.
-// Parameters are specified after the function name, inside the parentheses.
-
-// The following example has a function that takes a string called fname as parameter.
-// When the function is called, we pass along a first name,
-// which is used inside the function to print the full name:
+// You can also use a default parameter value, by using the equals sign (=).
+// If we call the function without an argument, it uses the default value ("Norway"):
 
 #include <iostream>
 using namespace std;
 
-void myFunction(string fname)
+void myFunction(string country = "Norway")
 {
-    cout << fname << "\n";
+    cout << country << "\n";
 }
 
 int main()
 {
-    myFunction("Liam");
-    myFunction("Jenny");
-    myFunction("Anna");
+    myFunction("Sweden");
+    myFunction("India");
+    myFunction();
+    myFunction("USA");
 
     return 0;
 }
 
-// When a parameter is passed to the function, it is called an argument.
-// So, from the example above: fname is a parameter,
-// while Liam, Jenny and Anna are arguments.
+// A parameter with a default value, is often known as an "optional parameter".
+// From the example above, country is an optional parameter and
+// "Norway" is the default value.
