3744d5b52ad5b5d9fd7cef01da9dc8176fb070e1
Suttipong Srikok <fisca2012@gmail.com>
2021-07-04 00:29:04 +0700

    Creating Pointers

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 int main()
 {
-    // Memory Address
+    // Creating Pointers
 
-    // When a variable is created in C++, a memory address is assigned to the variable. And when we assign a value to the variable, it is stored in this memory address.
-    // To access it, use the & operator, and the result will represent where the variable is stored:
+    // A pointer however, is a variable that stores the memory address as its value.
+    // A pointer variable points to a data type (like int or string) of the same type, and is created with the * operator. The address of the variable you're working with is assigned to the pointer:
 
-       string food = "Pizza";
+    string food = "Pizza";  // A food variable of type string
+    string* ptr = &food;    // A pinter variable, with the name ptr, that stores the address of food
 
-       cout << &food;
+    // Output the value of food (Pizza)
+    cout << food << "\n";
+
+    // Output the memory address of food
+    cout << &food << "\n";
+
+    // Output the memory address of food with the pointer
+    cout << ptr << "\n";
 
     return 0;
 }
+
+// Tip: There are three ways to declare pointer variables, but the first way is preferred:
+// string* mystring; // Preferred
+// string *mystring;
+// string * mystring;
