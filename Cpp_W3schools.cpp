636914a31c8c1dc4aadf2e1b5eaf244365852211
Suttipong Srikok <fisca2012@gmail.com>
2021-07-24 22:16:34 +0700

    Pass By Reference.

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,17 +1,28 @@
-// Return Values
+// Pass By Reference
 
 #include <iostream>
 using namespace std;
 
-int myFunction(int x, int y)
+void swapNums(int &x, int &y)
 {
-    return x + y;
+    int z = x;
+    x = y;
+    y = z;
 }
 
 int main()
 {
-    int z = myFunction(5, 3);
-    cout << z;
+    int firstNum = 10;
+    int secondNum = 20;
+
+    cout << "Before swap: " << "\n";
+    cout << firstNum << secondNum << "\n";
+
+    // Call the function, which will change the values of firstNum and secondNum
+    swapNums(firstNum, secondNum);
+
+    cout << "After swap: " << "\n";
+    cout << firstNum << secondNum << "\n";
 
     return 0;
 }
