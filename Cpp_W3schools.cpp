ab7fed61081dda4c7d54fd065cf4dcf4b4d2de38
Suttipong Srikok <fisca2012@gmail.com>
2021-07-28 23:42:06 +0700

    Class Methods - Inside class definition

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// Create a Car class with some attributes
-class Car
+class MyClass       // The class
 {
-public:
-    string brand;
-    string model;
-    int year;
+public:             // Access specifier
+    void myMethod() // Method/function defined inside the class
+    {
+        cout << "Hello World";
+    }
 };
 
 int main()
 {
-    // Create an object of Car
-    Car carObj1;
-    carObj1.brand = "BMW";
-    carObj1.model = "X5";
-    carObj1.year = 1999;
-
-    // Create another object of Car
-    Car carObj2;
-    carObj2.brand = "Ford";
-    carObj2.model = "Mustang";
-    carObj2.year = 1969;
-
-    // Print attribute values
-    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
-    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+    MyClass myObj;      // Create an object of MyClass
+    myObj.myMethod();   // Call the method
 
     return 0;
 }
