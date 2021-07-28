f1615c89e0a08315947c6bcecd37de3a508dda68
Suttipong Srikok <fisca2012@gmail.com>
2021-07-28 23:34:05 +0700

    Multiple Objects

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class MyClass           // The class
+// Create a Car class with some attributes
+class Car
 {
-public:                 // Access specifier
-    int myNum;          // Attribute (int variable)
-    string myString;    // Attribute (string variable)
+public:
+    string brand;
+    string model;
+    int year;
 };
 
 int main()
 {
-    MyClass myObj;  // Create an object of MyClass
+    // Create an object of Car
+    Car carObj1;
+    carObj1.brand = "BMW";
+    carObj1.model = "X5";
+    carObj1.year = 1999;
 
-    // Access attribute and set values
-    myObj.myNum = 15;
-    myObj.myString = "Some Text";
+    // Create another object of Car
+    Car carObj2;
+    carObj2.brand = "Ford";
+    carObj2.model = "Mustang";
+    carObj2.year = 1969;
 
     // Print attribute values
-    cout << myObj.myNum << "\n";
-    cout << myObj.myString;
+    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
 
     return 0;
 }
