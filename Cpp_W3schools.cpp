f90662f08773260bb65384ea634d1f2f8123c9b6
Suttipong Srikok <fisca2012@gmail.com>
2021-07-28 23:45:42 +0700

    Class Methods - Outside class definition

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -4,11 +4,14 @@ using namespace std;
 class MyClass       // The class
 {
 public:             // Access specifier
-    void myMethod() // Method/function defined inside the class
-    {
-        cout << "Hello World";
-    }
+    void myMethod(); // Method/function defined inside the class
 };
+
+// Method/function definition outside the class
+void MyClass::myMethod()
+{
+    cout << "Hello World!";
+}
 
 int main()
 {
