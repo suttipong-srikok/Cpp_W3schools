a6e0c49de44c353bebcd89d186e4825d0bee0f17
Suttipong Srikok <fisca2012@gmail.com>
2021-07-29 23:58:10 +0700

    Constructor Parameters

--- Cpp_W3schools.cpp
+++ Cpp_W3schools.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class MyClass           // The Class
+class Car           // The Class
 {
-public:                 // Access specifier
-    MyClass()           // Constructor
-    {
-        cout << "Hello World!";
-    }
+public:             // Access specifier
+    string brand;   // Attribute
+    string model;   // Attribute
+    int year;       // Attribute
+    Car(string x, string y, int z);  // Constructor declaration
 };
+
+// Constructor definition outside the class
+Car::Car(string x, string y, int z)
+{
+    brand = x;
+    model = y;
+    year = z;
+}
 
 int main()
 {
-    MyClass myObj;  // Create an object of MyClass (this will call the constructor)
+    // Create Car Objects and call the constructor with different value
+    Car carObj1("BMW", "X5", 1999);
+    Car carObj2("Ford", "Mustang", 1969);
+
+    // Print values
+    cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+    cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+
     return 0;
 }
